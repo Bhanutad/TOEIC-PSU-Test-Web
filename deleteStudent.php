1e3c128d4035c625ed9f664edde9fc05f72b8abc
Bhanutad Arjhan <Bhanutadarjhan@gmail.com>
2020-10-06 22:09:58 +0700

    delete student from the database

---
+++ deleteStudent.php
@@ -0,0 +1,23 @@
+<?php
+include  'config.php';
+session_start();
+		$studentID = $_GET["studentID"];
+       
+
+        $sql = "DELETE FROM student WHERE studentID=$studentID ";
+                $result = mysqli_query($connect,$sql);
+                mysqli_close($connect);
+
+
+                if($result){
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Successfully deleted admin.');";
+                    echo "window.location = 'studentlist.php'; ";
+                    echo "</script>";
+                    }
+                    else{
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Something went wrong Please try again.);";
+                    echo "</script>";
+                }
+?>
