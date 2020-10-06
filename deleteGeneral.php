7e64a56206e2bbed18f680ab9768c4c3db1aff87
Bhanutad Arjhan <Bhanutadarjhan@gmail.com>
2020-10-06 22:10:16 +0700

    delete general from the database

---
+++ deleteGeneral.php
@@ -0,0 +1,23 @@
+<?php
+include  'config.php';
+session_start();
+		$genaralID = $_GET["generalID"];
+       
+
+        $sql = "DELETE FROM general WHERE generalID=$generalID ";
+                $result = mysqli_query($connect,$sql);
+                mysqli_close($connect);
+
+
+                if($result){
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Successfully deleted admin.');";
+                    echo "window.location = 'general.php'; ";
+                    echo "</script>";
+                    }
+                    else{
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Something went wrong Please try again.);";
+                    echo "</script>";
+                }
+?>
