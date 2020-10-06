26b5191677eec9a601668a17edbc94b00977c269
Bhanutad Arjhan <Bhanutadarjhan@gmail.com>
2020-10-07 02:04:26 +0700

    update status to the database

---
+++ updateStatus.php
@@ -0,0 +1,23 @@
+<?php 
+include  'config.php';
+$examID = $_GET['examID'];
+                $sql = "UPDATE exam SET statusShow = 0
+                WHERE examID ='$examID' ";
+                $result = mysqli_query($connect,$sql);
+                mysqli_close($connect);
+
+
+                if($result){
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Successfully confirmed.');";
+                    echo "window.location = 'home.php'; ";
+                    echo "</script>";
+                    }
+                    else{
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Something went wrong Please try again.');";
+                    echo "window.location = 'home.php'; ";
+                    echo "</script>";
+                }
+ 
+?>
