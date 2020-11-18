d8d87646993df105169de9f976a751da5f4c4a2d
Bhanutad Arjhan <Bhanutadarjhan@gmail.com>
2020-11-18 18:51:48 +0700

    coding to update term and con to the database

--- term.php
+++ term.php
@@ -10,104 +10,88 @@ include 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
     <style>
-      table {
-        width: 1120px; 
-        border: 3px solid gray;
-        border-collapse: collapse;
-        text-align: left;
-      }
-      th{
-        border-collapse: collapse;
-        width: 140px;
-        padding: 5px;
-        text-align: left;
-        font-size: 15px;
-      }
-      td{
-        border: 1px solid gray;
-        font-size: 10px;
-      }
+        .box{
+          margin-top: 10px;
+          margin-bottom: 10px;
+            border: 5px solid black;
+            text-align: left;
+        }
+      body.modal-open .modal {
+      margin-top: 60px;
+} 
     </style>
   </head>
   <body>
-      <div>
-          <div>
-              <h1 class="display-2">Term and Conditions</h1>
-          </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-md-3"></div>
-              <div class="col-md-6">
-                <br>
-                <table class="table">
-                  <br>
+    <div>
+        <div>
+            <h1 class="display-2">Term and Conditions</h1>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-2"></div>
+            <div class="box col-md-8">
+			<br>
                   <tbody>
                     <?php
                     include  'config.php';
 
-                    $sqladmin = 'SELECT * FROM term';
-                    $resultadmin = mysqli_query($connect,$sqladmin);
-                    while($row= mysqli_fetch_array( $resultadmin, MYSQLI_ASSOC)){
+                    $sqlterm = 'SELECT * FROM term ';
+                    $resultterm = mysqli_query($connect,$sqlterm);
+                    while($row= mysqli_fetch_array( $resultterm, MYSQLI_ASSOC)){
                     ?>
                     <tr>
-                      <td> <?php echo "$row[term]" ?></td>
-                      <td>
-                        <div >
-
-                        <!-- echo "<a href='#' >$row[file]</a> ".'<br>';  -->
-                        <a href="#" class="edit-term btn btn-info btn-lg"
-                      data-term="<?php echo $row['term']?>">Edit 
-                          </a>
-                        </div>
+					              <td> <?php echo "$row[text]" ?></td>
                         <br>
-                        <div>
-                        <a href="">
-                      <button type="button" name="button" class="btn btn-danger btn-lg">Delete</button>
-                          </a>
-                          </div>
-                      </td>
                     </tr>
+                  </tbody>
+                <div style="text-align: center;">
+                        <a href="#" class="edit-term btn btn-info btn-lg"
+                        data-termID="<?php echo $row['termID']?>"
+                      data-text="<?php echo $row['text']?>">Edit 
+                        </a>
+                </div>
                     <?php
                     }
                     ?>
-                  </tbody>
-                </table>
               </div>
-              <div class="col-md-3"></div> 
-            </div>
-          </div>
+          <div class="col-md-2"></div>
+        </div>
+	</div>
 </div>
-<!-- Modal -->
+	<!-- Modal -->
 <div class="modal fade" id="formEditTerm">
             <div class="modal-dialog">
-              <form action=".php" method="post" >
                 <div class="modal-content">
+                <form action="updateTerm.php" method="post" >
                   <div class="modal-header">
-                    <!-- <button type="button" class="close" data-dismis="modal" name="button">
-                    <span aria-hidden="true">&times;</span>
-                    </button> -->
                   </div>
                   <div class="modal-body">
-                            </div>
-                            <div class="form-group">
-				                <label class="control-label col-sm-4">News:</label>
+                  <div class="form-group">
+				                <label class="control-label col-sm-4">Term ID:</label>
 				                <div class="col-sm-10">          
-					                <textarea name="text" id="term" cols="30" rows="10"></textarea>
+					                <input type="text" name="termID" id="termID" readonly>
 				                </div>
+                            <div class="form-group">
+				                <label class="control-label col-sm-4">Term and Condition:</label>
+				                <div class="col-sm-10">  
+									<textarea name="text" id="text" cols="50" rows="10" required ></textarea>       
+				                </div>
+                            </div>
+                </div>
                 </div>
                   <div class="modal-footer">
                   <div style="text-align: center;">
-                  <input type="submit" class="btn btn-primary btn-lg " value="Cancel">
+                  <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Cancel</button>
                 <input type="submit" class="btn btn-success btn-lg " value="Save">
                 </div>
                   </div>
-                </div>
               </form>
-            </div>
           </div>
+  </div>
   </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -115,16 +99,17 @@ include 'header.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
-      $('.edit-term').click(function(){
+    $('.edit-term').click(function(){
        
        // // get data from edit btn
-         var term= $ (this).attr('data-term');
+         var termID= $ (this).attr('data-termID');
+         var text= $ (this).attr('data-text');
        // // set value to modal
-         $('#term').val(term);
+         $('#termID').val(termID);
+         $('#text').val(text);
          
        $('#formEditTerm').modal('show');
      });
-  
     </script>
   </body>
 </html>
---
+++ updateTerm.php
@@ -0,0 +1,39 @@
+<script>
+
+
+s
+
+</script>
+
+<?php 
+include  'config.php';
+$termID = $_POST['termID'];
+$text = $_POST['text'];
+
+
+
+$text =  mysqli_real_escape_string($connect,$text);
+ 
+
+
+
+                $sql = "UPDATE term SET  text ='$text'
+                WHERE termID = '$termID'";
+                $result = mysqli_query($connect,$sql);
+                mysqli_close($connect);
+                // echo $sql;
+
+                if($result){
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Successfully updated term and condition.');";
+                    echo "window.location = 'term.php'; ";
+                    echo "</script>";
+                    }
+                    else{
+                    echo "<script type='text/javascript'>";
+                    echo "alert('Something went wrong Please try again.');";
+                    echo "window.location = 'term.php'; ";
+                    echo "</script>";
+                }
+ 
+?>
