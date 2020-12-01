9a67b148077196ba60189719280967a3280b0616
Bhanutad Arjhan <Bhanutadarjhan@gmail.com>
2020-12-01 20:11:30 +0700

    edit

--- addNews2.php
+++ addNews2.php
@@ -4,19 +4,7 @@ $head = $_POST['head'];
 $text = $_POST['text'];
 $file = (isset($_POST['fileupload']) ? $_POST['fileupload'] : '');
 $upload=$_FILES['fileupload'];
-// if($upload != '') { 
-//     $path="image/";  
-//     $remove_these = array(' ','`','"','\'','\\','/','_');
-// 	$newname = str_replace($remove_these, '', $_FILES['fileupload']['name']);
- 
-// 	//ตั้งชื่อไฟล์ใหม่โดยเอาเวลาไว้หน้าชื่อไฟล์เดิม
-// 	$newname = time().'-'.$newname;
-//     $path_copy=$path.$newname;
-//     $path_link="fileupload/".$newname;
 
-//     //คัดลอกไฟล์ไปเก็บที่เว็บเซิฟเวอร์
-//     move_uploaded_file($_FILES['fileupload']['tmp_name'],$path_copy);
-// }
 $time = time();
 $data =date("Ymd"); //random date
 $path ="image/";
